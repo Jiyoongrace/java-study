2357651c1959f25fac7bff8887a355dac1672680
Jiyoongrace <giyun3632@naver.com>
2024-04-29 14:18:47 +0900

    fix: design pattern - Singleton

--- designpattern-practices/src/main/java/creational/singleton/Singleton.java
+++ designpattern-practices/src/main/java/creational/singleton/Singleton.java
@@ -5,6 +5,7 @@ public class Singleton {
 
     private Singleton() {}; // private 생성자
 
+    // 객체 반환 정적 메서드
     public static Singleton getInstance() {
         if (instance == null) {
             instance = new Singleton();
