2dab608c518a1c1763b3c2fc8be404bfd8ba9377
Jiyoongrace <giyun3632@naver.com>
2024-05-01 15:43:32 +0900

    feat: network TCPServer, TCPClient

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -1,9 +1,12 @@
 package test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class TCPServer {
     public static void main(String[] args) {
@@ -21,7 +24,53 @@ public class TCPServer {
 
             // 3. accept
             Socket socket = serverSocket.accept(); // blocking
-            System.out.println("연결 성공");
+            // System.out.println("연결 성공");
+            // ----- 여기까지 서버 소켓
+
+            // 여기부터 클라이언트
+            // Client IP Address + Port
+            try {
+                InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+                String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+                int remotePort = inetRemoteSocketAddress.getPort();
+                System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remotePort + "]");
+
+                // 4. IO Stream 받아오기
+                InputStream is = socket.getInputStream();
+                OutputStream os = socket.getOutputStream();
+
+                while(true) {
+                    // 5. 데이터 읽기
+                    byte[] buffer = new byte[256];
+                    int readByteCount = is.read(buffer); // blocking
+
+                    if(readByteCount == -1) {
+                        // 클라이언트가 정상적으로 종료(close() 호출)
+                        System.out.println("[server] closed by client");
+                        break;
+                    }
+                    String data = new String(buffer, 0, readByteCount, "utf-8");
+                    System.out.println("[server] received: " + data);
+
+                    // 6. 데이터 쓰기
+                    os.write(data.getBytes("utf-8"));
+                }
+                // cmd 창에서 실행 가능
+                // C:\poscodx2024\java-study\network\target\classes>java test.TCPServer
+
+            } catch (SocketException e) {
+                System.out.println("[server] suddenly closed by client");
+            } catch (IOException e) {
+                System.out.println("[server] error: " + e);
+            } finally {
+                try {
+                    if(socket != null && !socket.isClosed()) {
+                        socket.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
         } catch (IOException e) {
             System.out.println("[server] error: " + e);
